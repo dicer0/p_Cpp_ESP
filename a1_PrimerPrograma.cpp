8c3c274badaa229a492edf795c2ff055fd0a9375
“di_cer0” <dcervantesr1401@alumno.ipn.mx>
2023-10-04 13:27:10 -0600

    MechaBible Mark I: Programming - C++ Spanish

---
+++ a1_PrimerPrograma.cpp
@@ -0,0 +1,12 @@
+/*Los lenguajes C y C++ son compilados, esto significa que requieren de un programa compilador que 
+convierta el código fuente en un archivo ejecutable que el ordenador interpreta como código binario.
+El proceso de compilación ocurre de la siguiente manera:
+ - Editor de texto: Contiene el código fuente .cpp o .h
+ - Preprocesador: Adjunta todos los archivos, imágenes, etc. que conformen al programa.
+ - Compilador: Convierte el código a un archivo objeto .obj o .o, que es un paso intermedio entre el 
+   código fuente y el código máquina.
+ - LInker: Se encarga de unificar el código objeto con las librerías o dependencias .lib o .a que esté 
+   utilizando el programa.
+ - Loader: Recibe un ejecutable en lenguaje máquina que interpreta el ordenador para ejecutar una 
+   acción.
+El compilador que usaremos para C++ se llama g++ y para instalarlo se utiliza la herramienta MinGW.*/
